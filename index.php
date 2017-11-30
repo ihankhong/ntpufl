bc8f789222027f9cbd30905311ca1801f3fdb97c
ihank <ihank@jetfuel.com.tw>
2017-11-30 10:16:52 +0800

    home banner 三張 已增加

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
  require_once("header.php");
  require_once("carousel.php");
 ?>
+<section class="banner-wrap">
+	<div class="container-full clearfix">
+		<div class="col-xs-12">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
+					<img src="images/home/banner-1.png" alt="banner-1.png" class="img-responsive center-block" />
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<img src="images/home/banner-2.png" alt="banner-2.png" class="img-responsive center-block" />
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<img src="images/home/banner-3.png" alt="banner-3.png" class="img-responsive center-block" />
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <section class="content-wrap">
 	<div class="container-full clearfix">
 		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
 			<div class="row">
 				<div class="col-xs-12 col-md-6">
 					<div class="homeblock-wrap">
-						<a href="#"><img src="images/home/homeblock-1.png" class="img-responsive center-block" alt="系辦公告" /></a>
 						<div class="list">
 							<a href="#">106學年第二學期停開課程</a>
 							<a href="#">University of California, Riverside(UCR)商學院</a>
@@ -20,7 +36,6 @@
 				</div>
 				<div class="col-xs-12 col-md-6">
 					<div class="homeblock-wrap">
-						<a href="#"><img src="images/home/homeblock-2.png" class="img-responsive center-block" alt="人才招募" /></a>
 						<div class="list">
 							<a href="#">106學年第二學期停開課程</a>
 							<a href="#">University of California, Riverside(UCR)商學院</a>
@@ -33,7 +48,6 @@
 				<div class="clearfix"></div>
 				<div class="col-xs-12 col-md-6">
 					<div class="homeblock-wrap">
-						<a href="#"><img src="images/home/homeblock-3.png" class="img-responsive center-block" alt="其他訊息" /></a>
 						<div class="list">
 							<a href="#">106學年第二學期停開課程</a>
 							<a href="#">University of California, Riverside(UCR)商學院</a>
@@ -45,7 +59,6 @@
 				</div>
 				<div class="col-xs-12 col-md-6">
 					<div class="homeblock-wrap">
-						<a href="#"><img src="images/home/homeblock-4.png" class="img-responsive center-block" alt="學術活動" /></a>
 						<div class="list">
 							<a href="#">106學年第二學期停開課程</a>
 							<a href="#">University of California, Riverside(UCR)商學院</a>
@@ -58,7 +71,7 @@
 			</div>
 		</div>
 		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
-			<div class="sidebar-wrap">
+			<!-- <div class="sidebar-wrap">
 				<h3>最新消息</h3>
 				<div class="list">
 					<div class="new"><a href="#">未來二十年最有價值的科系：資工系</a></div>
@@ -74,7 +87,7 @@
 					<div class="new"><a href="#">國立臺北大學資訊工程學系通過IEET</a></div>
 					<div class="new"><a href="#">資訊工程學系碩士班(含學班)演講時程</a></div>
 				</div>
-			</div>
+			</div> -->
 		</div>
 	</div>
 </section>
