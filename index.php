7326ebca09fdf56b15124e7608b0ee15c5116c93
ihank <ihank@jetfuel.com.tw>
2017-11-30 18:59:15 +0800

    首頁content 區塊 完成

--- index.php
+++ index.php
@@ -22,6 +22,70 @@
 <section class="content-wrap">
 	<div class="container-full clearfix">
 		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+			<div class="homeblock-wrap clearfix">
+				<h3>系所公告<a href="#" class="pull-right">View All</a></h3>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">專題講座106.11.3新手譯者如何主動接到第一個案子</a></div>
+				</div>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">專題講座-105.12.21公關宣傳及文案撰寫</a></div>
+				</div>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">專題講座--105.12.27二十一世紀中文教師教學法則點滴</a></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+			<div class="col-xs-12">
+				<div class="homeside-img">
+					<img src="images/home/sidebar/side.png" class="img-responsive center-block" />
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+			<div class="homeblock-wrap clearfix">
+				<h3>學術活動<a href="#" class="pull-right">View All</a></h3>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">106學年度「發音練習」課程選修學生名單</a></div>
+				</div>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">106學年度第1學期輔系、雙主修申請考試公告</a></div>
+				</div>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">教學B-1深化國際移動專業學習錄取名單及行前說明會</a></div>
+				</div>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">教學B-1深化國際移動專業學習</a></div>
+				</div>
+				<div class="list clearfix">
+					<div class="col-xs-5"><span class="btn category">專題講座</span><span class="date">2017-12-30</span></div>
+					<div class="col-xs-7"><a href="#">2017畢業公演</a></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+			<div class="col-xs-12">
+				<div class="homeside-img">
+					<img src="images/home/sidebar/side.png" class="img-responsive center-block" />
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+ require_once("footer.php");
+?>
+
+<!-- <section class="content-wrap">
+	<div class="container-full clearfix">
+		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
 			<div class="row">
 				<div class="col-xs-12">
 					<div class="homeblock-wrap clearfix">
@@ -68,26 +132,6 @@
 			</div>
 		</div>
 		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
-			<!-- <div class="sidebar-wrap">
-				<h3>最新消息</h3>
-				<div class="list">
-					<div class="new"><a href="#">未來二十年最有價值的科系：資工系</a></div>
-					<div class="new"><a href="#">國立臺北大學資訊工程學系通過IEET</a></div>
-					<div class="new"><a href="#">資訊工程學系碩士班(含學班)演講時程</a></div>
-					<div class="new"><a href="#">未來二十年最有價值的科系：資工系</a></div>
-					<div class="new"><a href="#">國立臺北大學資訊工程學系通過IEET</a></div>
-					<div class="new"><a href="#">資訊工程學系碩士班(含學班)演講時程</a></div>
-					<div class="new"><a href="#">未來二十年最有價值的科系：資工系</a></div>
-					<div class="new"><a href="#">國立臺北大學資訊工程學系通過IEET</a></div>
-					<div class="new"><a href="#">資訊工程學系碩士班(含學班)演講時程</a></div>
-					<div class="new"><a href="#">未來二十年最有價值的科系：資工系</a></div>
-					<div class="new"><a href="#">國立臺北大學資訊工程學系通過IEET</a></div>
-					<div class="new"><a href="#">資訊工程學系碩士班(含學班)演講時程</a></div>
-				</div>
-			</div> -->
 		</div>
 	</div>
-</section>
-<?php
- require_once("footer.php");
-?>
+</section> -->
